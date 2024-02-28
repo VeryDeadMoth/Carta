dc93498b16d5d107521db55a627d0f25fd54bce5
VeryDeadMoth <nix.axius@gmail.com>
2024-02-28 15:29:51 +0100

    Moved Quiz Question class into Deserializer

--- CARTAPENTA/Assets/Scripts/Deserializer/Deserializer.cs
+++ CARTAPENTA/Assets/Scripts/Deserializer/Deserializer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,15 @@ using UnityEngine;
 public class Deserializer 
 {
     public Dictionary<string, List<QuizQuestion>> GetAllQuizQuestions(TextAsset file) => Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, List<QuizQuestion>>>(file.text);
+
+
+}
+
+//classes that need to be deserialized
+
+[Serializable]
+public class QuizQuestion
+{
+    public string text;
+    public bool isCorrectAnswer;
 }
