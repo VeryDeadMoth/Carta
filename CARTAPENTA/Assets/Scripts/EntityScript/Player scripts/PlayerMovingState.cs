ec5a7549bf54cf6ea02ce26dac5fb4e81bd3c83d
Samoht <thomthom.lange@gmail.com>
2024-03-05 12:20:15 +0100

    CloudEffect added

--- CARTAPENTA/Assets/Scripts/EntityScript/Player scripts/PlayerMovingState.cs
+++ CARTAPENTA/Assets/Scripts/EntityScript/Player scripts/PlayerMovingState.cs
@@ -32,6 +32,7 @@ public class PlayerMovingState : PlayerBaseState
         if(Input.GetMouseButtonDown(0))
         {
             player.targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
             Debug.Log("Mouse Input -> new direction : " + player.targetPos);
 
             //Flip player sprite
