9419638f44306c8f6217663dabb8ebf3864dae13
Samoht <thomthom.lange@gmail.com>
2024-03-05 12:20:15 +0100

    CloudEffect added

---
+++ CARTAPENTA/Assets/Scripts/EntityScript/CloudEffect/CloudEffect.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Effects;
+
+public class CloudEffect : MonoBehaviour
+{
+    [SerializeField] private GameObject ColliderRestriction;
+    public int numZone { get; set; } = 0;
+    private fadeEffects fadeEffects;
+
+    private void Start()
+    {
+        fadeEffects = new fadeEffects();
+    }
+    private void Update()
+    {
+        if (numZone != -1)
+        {
+            if (ColliderRestriction != null)
+            {
+                if (ColliderRestriction.transform.childCount > numZone)
+                {
+                    GameObject CloudsGroup = ColliderRestriction.transform.GetChild(numZone).gameObject;
+                    
+                    if (!fadeEffects.EndTransitionFadeGroup(CloudsGroup))
+                    {
+                        fadeEffects.FadeOutAllObject(CloudsGroup, 0);
+                    }
+                }
+            }
+        }
+    }
+
+}
